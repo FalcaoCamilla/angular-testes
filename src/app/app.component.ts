a7e6f40e28275bf7e7780a4bea5167ea685a631b
FalcaoCamilla <millafalcao.acad@gmail.com>
2023-02-24 19:21:38 -0300

    first unit test - sum function

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-testes';
+
+  public soma(value1: number, value2: number){
+    return value1 + value2;
+  }
+}
